bdb261c9cb591269d4ad18f20552a7562c413ce2
Lapwpkddm <poporaum20@gmail.com>
2026-02-15 19:42:14 +1000

    Update ScriptMobile.lua

--- Online Script/ScriptMobile.lua
+++ Online Script/ScriptMobile.lua
@@ -7566,6 +7566,106 @@ task.spawn(function()
 end)
 
 -- Добавляем новую вкладку Event, если её ещё нет
+local EventTab = Window:AddTab({ Title = "Event", Icon = "calendar" })
+
+EventTab:AddSection("Battlepass Modifications")
+
+-- Первый тумблер: Unlocked Pass
+local UnlockedPassToggle = EventTab:AddToggle("UnlockedPassToggle", {
+    Title = "Open Unlocked Pass",
+    Default = false,
+    Callback = function(state)
+        if state then
+            -- Включаем скрытие
+            if not featureStates.UnlockedPassLoop then
+                featureStates.UnlockedPassLoop = RunService.Heartbeat:Connect(function()
+                    pcall(function()
+                        local menu = game:GetService("Players").LocalPlayer.PlayerGui.Menu
+                        if menu and menu.Views and menu.Views.Battlepass and menu.Views.Battlepass.ViewPass then
+                            local center = menu.Views.Battlepass.ViewPass.Center
+                            if center and center.ViewPass and center.ViewPass.Unlocked then
+                                center.ViewPass.Unlocked.Visible = false
+                            end
+                        end
+                    end)
+                end)
+            end
+        else
+            -- Отключаем скрытие
+            if featureStates.UnlockedPassLoop then
+                featureStates.UnlockedPassLoop:Disconnect()
+                featureStates.UnlockedPassLoop = nil
+                
+                -- Восстанавливаем видимость
+                pcall(function()
+                    local menu = game:GetService("Players").LocalPlayer.PlayerGui.Menu
+                    if menu and menu.Views and menu.Views.Battlepass and menu.Views.Battlepass.ViewPass then
+                        local center = menu.Views.Battlepass.ViewPass.Center
+                        if center and center.ViewPass and center.ViewPass.Unlocked then
+                            center.ViewPass.Unlocked.Visible = true
+                        end
+                    end
+                end)
+            end
+        end
+    end
+})
+
+-- Второй тумблер: Exchange Open
+local ExchangeOpenToggle = EventTab:AddToggle("ExchangeOpenToggle", {
+    Title = "Open Exchange",
+    Default = false,
+    Callback = function(state)
+        if state then
+            -- Включаем открытие Exchange
+            if not featureStates.ExchangeOpenLoop then
+                featureStates.ExchangeOpenLoop = RunService.Heartbeat:Connect(function()
+                    pcall(function()
+                        local menu = game:GetService("Players").LocalPlayer.PlayerGui.Menu
+                        if menu and menu.Views and menu.Views.Battlepass then
+                            -- Показываем Exchange
+                            if menu.Views.Battlepass.Exchange then
+                                menu.Views.Battlepass.Exchange.Visible = true
+                            end
+                            
+                            -- Скрываем ViewPass
+                            if menu.Views.Battlepass.ViewPass then
+                                menu.Views.Battlepass.ViewPass.Visible = false
+                            end
+                        end
+                    end)
+                end)
+            end
+        else
+            -- Отключаем
+            if featureStates.ExchangeOpenLoop then
+                featureStates.ExchangeOpenLoop:Disconnect()
+                featureStates.ExchangeOpenLoop = nil
+                
+                -- Восстанавливаем стандартный вид
+                pcall(function()
+                    local menu = game:GetService("Players").LocalPlayer.PlayerGui.Menu
+                    if menu and menu.Views and menu.Views.Battlepass then
+                        -- Восстанавливаем Exchange
+                        if menu.Views.Battlepass.Exchange then
+                            menu.Views.Battlepass.Exchange.Visible = false
+                        end
+                        
+                        -- Восстанавливаем ViewPass
+                        if menu.Views.Battlepass.ViewPass then
+                            menu.Views.Battlepass.ViewPass.Visible = true
+                        end
+                    end
+                end)
+            end
+        end
+    end
+})
+
+EventTab:AddParagraph({
+    Title = "Примечание",
+    Content = "Эти функции изменяют отображение интерфейса Battlepass. Отключите их перед закрытием игры."
+})
 
 local InfoTab = Window:AddTab({ Title = "Info", Icon = "help-circle" })
 
@@ -7940,3 +8040,4 @@ LocalPlayer.CharacterAdded:Connect(function()
     end
 
 end)
+
