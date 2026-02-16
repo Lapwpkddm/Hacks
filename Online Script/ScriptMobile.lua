caebd7718aba79e6d06594e575605f11f6ad1cde
Lapwpkddm <poporaum20@gmail.com>
2026-02-16 23:27:51 +1000

    Update ScriptMobile.lua

--- Online Script/ScriptMobile.lua
+++ Online Script/ScriptMobile.lua
@@ -1,3 +1,4 @@
+game:GetService("Players").LocalPlayer.PlayerGui.DraconicHubGui:Destroy()
 game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "💖Draconic Hub X Evade💖",
     Text = "Welcome Draconic Hub Remake",
@@ -7922,5 +7923,6 @@ LocalPlayer.CharacterAdded:Connect(function()
     end
 
 end)
+
 
 
